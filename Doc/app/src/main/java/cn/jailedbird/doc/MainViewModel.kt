de0dbc1ebbf73729ce5c7431fc726a2e37aea8a8
JailedBird <zjc986812982@gmail.com>
2023-09-02 21:38:44 +0800

    feat: 修复依赖；

--- Doc/app/src/main/java/cn/jailedbird/doc/MainViewModel.kt
+++ Doc/app/src/main/java/cn/jailedbird/doc/MainViewModel.kt
@@ -1,7 +1,18 @@
 package cn.jailedbird.doc
 
 import androidx.lifecycle.ViewModel
+import retrofit2.Retrofit
+
+interface MainApiService {
+
+}
 
 class MainViewModel : ViewModel() {
+
+    init {
+        val retrofit = Retrofit.Builder().baseUrl("").build()
+        retrofit.create(MainApiService::class.java)
+    }
+
 
 }
